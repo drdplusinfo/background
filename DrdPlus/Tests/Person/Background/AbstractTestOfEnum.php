eb3d4c1ff42c8897b9d29d48c2e2368c163907c4
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-12-11 12:01:39 +0100

    Removed unused professions library

--- DrdPlus/Tests/Person/Background/AbstractTestOfEnum.php
+++ DrdPlus/Tests/Person/Background/AbstractTestOfEnum.php
@@ -3,7 +3,7 @@ namespace DrdPlus\Tests\Person\Background;
 
 use Doctrineum\Scalar\ScalarEnum;
 use Doctrineum\Scalar\ScalarEnumType;
-use DrdPlus\Tools\Tests\TestWithMockery;
+use DrdPlus\Tests\Tables\Measurements\TestWithMockery;
 
 abstract class AbstractTestOfEnum extends TestWithMockery
 {
