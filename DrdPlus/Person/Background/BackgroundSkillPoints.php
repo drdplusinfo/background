7028e0a0bac3d90132f959594b598e6072eedcba
jaroslavtyc <mail@jaroslavtyc.com>
2015-12-14 09:18:41 +0100

    Background skill points has a getter per skill type

--- DrdPlus/Person/Background/BackgroundSkillPoints.php
+++ DrdPlus/Person/Background/BackgroundSkillPoints.php
@@ -1,5 +1,6 @@
 <?php
 namespace DrdPlus\Person\Background;
+use DrdPlus\Codes\SkillCodes;
 use DrdPlus\Person\Background\Parts\AbstractHeritageDependent;
 use DrdPlus\Tables\Tables;
 
@@ -23,5 +24,34 @@ class BackgroundSkillPoints extends AbstractHeritageDependent
             $professionName,
             $skillType
         );
+    }
+
+    /**
+     * @param string $professionName
+     * @param Tables $tables
+     * @return int
+     */
+    public function getPhysicalSkillPoints($professionName, Tables $tables)
+    {
+        return $this->getSkillPoints($professionName, SkillCodes::PHYSICAL, $tables);
+    }
+    /**
+     * @param string $professionName
+     * @param Tables $tables
+     * @return int
+     */
+    public function getPsychicalSkillPoints($professionName, Tables $tables)
+    {
+        return $this->getSkillPoints($professionName, SkillCodes::PSYCHICAL, $tables);
+    }
+
+    /**
+     * @param string $professionName
+     * @param Tables $tables
+     * @return int
+     */
+    public function getCombinedSkillPoints($professionName, Tables $tables)
+    {
+        return $this->getSkillPoints($professionName, SkillCodes::COMBINED, $tables);
     }
 }
--- DrdPlus/Tests/Person/Background/BackgroundSkillPointsTest.php
+++ DrdPlus/Tests/Person/Background/BackgroundSkillPointsTest.php
@@ -10,8 +10,9 @@ class BackgroundSkillPointsTest extends AbstractTestOfHeritageDependent
 {
     /**
      * @test
+     * @dataProvider provideSkillType
      */
-    public function I_can_get_skill_points()
+    public function I_can_get_skill_points($skillType)
     {
         $backgroundPoints = $this->createBackgroundPoints($pointsValue = 7);
         $heritage = $this->createHeritage($heritageValue = 456);
@@ -19,22 +20,47 @@ class BackgroundSkillPointsTest extends AbstractTestOfHeritageDependent
         $backgroundSkills = BackgroundSkillPoints::getIt($backgroundPoints, $heritage);
         $tables = $this->mockery(Tables::class)
             ->shouldReceive('getBackgroundSkillsTable')
-            ->once()
+            ->atLeast()->once()
             ->andReturn($backgroundSkillsTable = $this->mockery(\stdClass::class))
             ->getMock();
         $backgroundSkillsTable->shouldReceive('getSkillPoints')
-            ->with($pointsValue, ProfessionCodes::FIGHTER, SkillCodes::COMBINED)
-            ->once()
+            ->with($pointsValue, ProfessionCodes::FIGHTER, $skillType)
+            ->atLeast()->once()
             ->andReturn($result = 'foo')
             ->getMock();
         /** @var Tables $tables */
         $this->assertSame(
             $result,
-            $backgroundSkills->getSkillPoints(
-                ProfessionCodes::FIGHTER,
-                SkillCodes::COMBINED,
-                $tables
-            )
+            $backgroundSkills->getSkillPoints(ProfessionCodes::FIGHTER, $skillType, $tables)
         );
+        switch ($skillType) {
+            case SkillCodes::PHYSICAL :
+                $this->assertSame(
+                    $result,
+                    $backgroundSkills->getPhysicalSkillPoints(ProfessionCodes::FIGHTER, $tables)
+                );
+                break;
+            case SkillCodes::PSYCHICAL :
+                $this->assertSame(
+                    $result,
+                    $backgroundSkills->getPsychicalSkillPoints(ProfessionCodes::FIGHTER, $tables)
+                );
+                break;
+            case SkillCodes::COMBINED :
+                $this->assertSame(
+                    $result,
+                    $backgroundSkills->getCombinedSkillPoints(ProfessionCodes::FIGHTER, $tables)
+                );
+                break;
+        }
+    }
+
+    public function provideSkillType()
+    {
+        return [
+            [SkillCodes::PHYSICAL],
+            [SkillCodes::PSYCHICAL],
+            [SkillCodes::COMBINED],
+        ];
     }
 }
