e75059122f0a0faf8f8912e919985221f1bc1191
Jaroslav Týc <mail@jaroslavtyc.com>
2017-02-17 08:30:58 +0100

    Fixed tests after a library incompatible change

--- DrdPlus/Tests/Background/BackgroundParts/BackgroundPartsExceptionsHierarchyTest.php
+++ DrdPlus/Tests/Background/BackgroundParts/BackgroundPartsExceptionsHierarchyTest.php
@@ -2,15 +2,21 @@
 namespace DrdPlus\Tests\Background\BackgroundParts;
 
 use DrdPlus\Background\Background;
-use Granam\Tests\Exceptions\Tools\AbstractExceptionsHierarchyTest;
+use Granam\Tests\ExceptionsHierarchy\Exceptions\AbstractExceptionsHierarchyTest;
 
-class ExceptionsHierarchyTest extends AbstractExceptionsHierarchyTest
+class BackgroundPartsExceptionsHierarchyTest extends AbstractExceptionsHierarchyTest
 {
+    /**
+     * @return string
+     */
     protected function getTestedNamespace()
     {
         return str_replace('\Tests', '', __NAMESPACE__);
     }
 
+    /**
+     * @return string
+     */
     protected function getRootNamespace()
     {
         $rootReflection = new \ReflectionClass(Background::class);
